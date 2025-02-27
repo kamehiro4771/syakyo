d36d4c34db0663b494d4320fb9b3a8815f5ee989
kamehiro4771 <kamehiro4771@outlook.jp>
2025-02-27 22:13:12 +0900

    C言語で学ぶアルゴリズムとデータ構造

---
+++ Untitled-1.c
@@ -0,0 +1,28 @@
+/*
+買ってから長らく放置していた「新・明解C言語で学ぶアルゴリズムとデータ構造第2版」を見て写経して行こうと思う
+特にデータ構造について理解を深めたい
+VSCodeの使い方も理解するために全てVSCodeを使ってプログラミングする
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    int max;
+    printf("みっつの整数の最大値を求めます。\n");
+    printf("aの値：");
+    scanf("%d", &a);
+    printf("bの値：");
+    scanf("%d", &b);
+    printf("cの値：");
+    scanf("%d", &c);
+
+    int max = a;
+    if(b > max)
+        max = b;
+    if(c > max)
+        max = c;
+    printf("最大値は%dです。\n", max);
+
+    return 0;
+}
