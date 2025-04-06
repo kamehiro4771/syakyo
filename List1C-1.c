90cbab8e4fc62813c780edc7a56c6ccdfa32e034
kamehiro4771 <kamehiro4771@outlook.jp>
2025-04-06 22:15:41 +0900

    文字化けしないようになった！

--- List1C-1.c
+++ List1C-1.c
@@ -18,6 +18,22 @@ int med3(int a, int b, int c)
     else
         return b;
 }
+
+int main(void)
+{
+    int a,b,c;
+
+    printf("みっつの整数の中央値を求めます。\n");
+    printf("aの値：");
+    scanf("%d",&a);
+    printf("bの値：");
+    scanf("%d",&b);
+    printf("c の値：");
+    scanf("%d",&c);
+
+    printf("中央値は%dです。\n",med3(a,b,c));
+    return 0;
+}
 /*3値の大小関係の組み合わせ13種類を列挙するのが、Fig.1C-4です。この図は、木の形（左端が根で
 そこから枝分かれする）であることから決定木（decision tree)とよばれます
 左端の枠（a>=b）からスタートして右側へと進みましょう。〇内の条件が成立すれば上側の線
